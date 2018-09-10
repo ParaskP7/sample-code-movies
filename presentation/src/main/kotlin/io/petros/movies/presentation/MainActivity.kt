462612ce3e1e91cd97ef3b441e19132610d8607d
Petros Paraskevopoulos <petros.paraskevopoulos@yahoo.com>
2018-09-10 15:01:24 +0200

    [movies] init: Add template main activity kotlin project

---
+++ presentation/src/main/kotlin/io/petros/movies/presentation/MainActivity.kt
@@ -0,0 +1,14 @@
+package io.petros.movies.presentation
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import io.petros.movies.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
