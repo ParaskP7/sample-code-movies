6c964342e2257a9c8122e572230a1989008724b3
Petros Paraskevopoulos <petros.paraskevopoulos@yahoo.com>
2018-12-01 17:11:46 +0100

    [movies] update: Update android studio to latest stable 3.2.1 version
    Also, as part of this update a lint error was fixed.

--- data/src/main/kotlin/io/petros/movies/data/network/rest/mapper/movie/MoviesMapper.kt
+++ data/src/main/kotlin/io/petros/movies/data/network/rest/mapper/movie/MoviesMapper.kt
@@ -1,13 +1,9 @@
 package io.petros.movies.data.network.rest.mapper.movie
 
 import android.content.Context
-import io.petros.movies.data.R
-import io.petros.movies.data.network.rest.response.movie.MovieResponse
 import io.petros.movies.data.network.rest.response.movie.MoviesResultPageResponse
 import io.petros.movies.domain.model.movie.Movie
 import io.petros.movies.domain.model.movie.MoviesResultPage
-import io.petros.movies.domain.toDate
-import io.petros.movies.domain.util.MOVIE_DATE_FORMAT
 
 class MoviesMapper { // MIT
 
@@ -29,16 +25,4 @@ class MoviesMapper { // MIT
 
     }
 
-}
-
-private fun MovieResponse.toMovie(context: Context): Movie {
-    return Movie(
-        id = id,
-        title = title,
-        releaseDate = release_date.toDate(MOVIE_DATE_FORMAT),
-        voteAverage = vote_average,
-        voteCount = vote_count,
-        overview = overview,
-        backdrop = backdrop_path?.let { context.getString(R.string.rest_themoviedb_image_url) + it }
-    )
 }
--- data/src/main/kotlin/io/petros/movies/data/network/rest/response/movie/MovieResponse.kt
+++ data/src/main/kotlin/io/petros/movies/data/network/rest/response/movie/MovieResponse.kt
@@ -1,5 +1,11 @@
 package io.petros.movies.data.network.rest.response.movie
 
+import android.content.Context
+import io.petros.movies.data.R
+import io.petros.movies.domain.model.movie.Movie
+import io.petros.movies.domain.toDate
+import io.petros.movies.domain.util.MOVIE_DATE_FORMAT
+
 data class MovieResponse(
     val vote_count: Int,
     val id: Int,
@@ -8,4 +14,18 @@ data class MovieResponse(
     val backdrop_path: String?,
     val overview: String,
     val release_date: String
-)
+) {
+
+    fun toMovie(context: Context): Movie {
+        return Movie(
+            id = id,
+            title = title,
+            releaseDate = release_date.toDate(MOVIE_DATE_FORMAT),
+            voteAverage = vote_average,
+            voteCount = vote_count,
+            overview = overview,
+            backdrop = backdrop_path?.let { context.getString(R.string.rest_themoviedb_image_url) + it }
+        )
+    }
+
+}
