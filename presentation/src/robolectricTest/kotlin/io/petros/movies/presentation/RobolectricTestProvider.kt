78fea92f6509e55dd76ebf908539e4d7a8d40ef8
Petros Paraskevopoulos <petros.paraskevopoulos@yahoo.com>
2018-12-08 21:19:56 +0100

    [movies] test: Replace deprecated imports for the robolectric provider

--- presentation/src/robolectricTest/kotlin/io/petros/movies/presentation/RobolectricTestProvider.kt
+++ presentation/src/robolectricTest/kotlin/io/petros/movies/presentation/RobolectricTestProvider.kt
@@ -1,14 +1,14 @@
 package io.petros.movies.presentation
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.test.core.app.ApplicationProvider
 import org.robolectric.Robolectric
-import org.robolectric.RuntimeEnvironment
 
 class RobolectricTestProvider {
 
     companion object {
 
-        internal fun provideContext() = RuntimeEnvironment.application.applicationContext
+        internal fun provideContext() = ApplicationProvider.getApplicationContext<App>()
 
         internal fun provideActivity() = Robolectric.setupActivity(AppCompatActivity::class.java)
 
