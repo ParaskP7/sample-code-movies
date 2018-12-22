999b6e5fd9be760a1531ae71ef490058792f7633
Petros Paraskevopoulos <petros.paraskevopoulos@yahoo.com>
2018-12-22 14:54:12 +0100

    [movies] update: Update kotlin to latest stable 1.3.11 version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,7 +5,7 @@ object Versions {
     object Plugin {
 
         const val ANDROID = "3.4.0-alpha09"
-        const val KOTLIN = "1.3.10"
+        const val KOTLIN = "1.3.11"
         const val VERSIONS = "0.20.0"
         const val DEXCOUNT = "0.8.4"
 
