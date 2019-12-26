944bc297013ce91b12bd45b2bb44b621e7281507
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2019-12-26 19:17:14 +0200

    [movies] docs: Add release link for android card view

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -71,6 +71,7 @@ object Versions {
         const val APP_COMPAT = "1.1.0"
         // Release: https://developer.android.com/jetpack/androidx/releases/recyclerview
         const val RECYCLER_VIEW = "1.1.0"
+        // Release: https://developer.android.com/jetpack/androidx/releases/cardview
         const val CARD_VIEW = "1.0.0"
         const val CONSTRAINT_LAYOUT = "1.1.3"
 
