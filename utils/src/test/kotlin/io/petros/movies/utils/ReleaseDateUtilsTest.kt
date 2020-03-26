7d4984769a17038adf74b7a7ed26493e051d8b6b
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-03-26 17:14:13 +0200

    [movies] analysis: Resolve maximum line length for utils test

--- utils/src/test/kotlin/io/petros/movies/utils/ReleaseDateUtilsTest.kt
+++ utils/src/test/kotlin/io/petros/movies/utils/ReleaseDateUtilsTest.kt
@@ -10,32 +10,32 @@ import strikt.assertions.isNull
 class ReleaseDateUtilsTest {
 
     @Test
-    fun `given no year, when converting to release date (greater than or equal), then null is returned`() {
+    fun `given no year, when converting (greater than or equal), then null is returned`() {
         expect { that(releaseDateGte(null, MOVIE_MONTH)).isNull() }
     }
 
     @Test
-    fun `given no year, when converting to release date (less than or equal), then null is returned`() {
+    fun `given no year, when converting (less than or equal), then null is returned`() {
         expect { that(releaseDateLte(null, MOVIE_MONTH)).isNull() }
     }
 
     @Test
-    fun `given year but no month, when converting year to release date (greater than or equal), then the expected range is returned`() {
+    fun `given year but no month, when converting year (greater than or equal), then expected range is returned`() {
         expect { that(releaseDateGte(MOVIE_YEAR, null)).isEqualTo("2018-01-01") }
     }
 
     @Test
-    fun `given year but no month, when converting year to release date (less than or equal), then the expected range is returned`() {
+    fun `given year but no month, when converting year (less than or equal), then expected range is returned`() {
         expect { that(releaseDateLte(MOVIE_YEAR, null)).isEqualTo("2018-12-31") }
     }
 
     @Test
-    fun `given year and month, When converting year and month to release date (greater than or equal), then the expected range is returned`() {
+    fun `given year and month, When converting year and month (greater than or equal), then expected range is returned`() {
         expect { that(releaseDateGte(MOVIE_YEAR, MOVIE_MONTH)).isEqualTo("2018-08-01") }
     }
 
     @Test
-    fun `given year and month, When converting year and month to release date (less than or equal), then the expected range is returned`() {
+    fun `given year and month, When converting year and month (less than or equal), then expected range is returned`() {
         expect { that(releaseDateLte(MOVIE_YEAR, MOVIE_MONTH)).isEqualTo("2018-08-31") }
     }
 
