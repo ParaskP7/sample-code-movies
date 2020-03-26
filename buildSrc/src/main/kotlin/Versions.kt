ebe15b6ff043866910332cfcf8fef702d9ebcab0
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-03-26 18:51:51 +0200

    [movies] refactor: Rename release comment to releases

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,23 +5,23 @@ object Versions {
 
     object Plugin {
 
-        // Release: https://androidstudio.googleblog.com
+        // Releases: https://androidstudio.googleblog.com
         const val ANDROID = "4.1.0-alpha04"
 
-        // Release: https://blog.jetbrains.com/kotlin/category/releases
+        // Releases: https://blog.jetbrains.com/kotlin/category/releases
         const val KOTLIN = "1.3.71"
 
-        // Release: https://github.com/arturbosch/detekt/releases
+        // Releases: https://github.com/arturbosch/detekt/releases
         const val DETEKT = "1.7.0"
 
-        // Release: https://github.com/mannodermaus/android-junit5/releases
+        // Releases: https://github.com/mannodermaus/android-junit5/releases
         const val ANDROID_J_UNIT_5 = "1.6.0.0"
 
     }
 
     object LeakCanary {
 
-        // Release: https://github.com/square/leakcanary/releases
+        // Releases: https://github.com/square/leakcanary/releases
         const val LEAK_CANARY = "2.2"
 
     }
@@ -30,7 +30,7 @@ object Versions {
 
     object Kotlin {
 
-        // Release: https://github.com/Kotlin/kotlinx.coroutines/releases
+        // Releases: https://github.com/Kotlin/kotlinx.coroutines/releases
         const val COROUTINES = "1.3.5"
 
     }
@@ -38,7 +38,7 @@ object Versions {
     @Suppress("MemberNameEqualsClassName")
     object Material {
 
-        // Release: https://github.com/material-components/material-components-android/releases
+        // Releases: https://github.com/material-components/material-components-android/releases
         const val MATERIAL = "1.1.0" // Released: 03-02-20
 
     }
@@ -47,23 +47,23 @@ object Versions {
 
         object Core {
 
-            // Release: https://developer.android.com/jetpack/androidx/releases/appcompat
+            // Releases: https://developer.android.com/jetpack/androidx/releases/appcompat
             const val APP_COMPAT = "1.1.0"
 
-            // Release: https://developer.android.com/jetpack/androidx/releases/recyclerview
+            // Releases: https://developer.android.com/jetpack/androidx/releases/recyclerview
             const val RECYCLER_VIEW = "1.1.0"
 
-            // Release: https://developer.android.com/jetpack/androidx/releases/cardview
+            // Releases: https://developer.android.com/jetpack/androidx/releases/cardview
             const val CARD_VIEW = "1.0.0"
 
-            // Release: https://developer.android.com/jetpack/androidx/releases/constraintlayout
+            // Releases: https://developer.android.com/jetpack/androidx/releases/constraintlayout
             const val CONSTRAINT_LAYOUT = "1.1.3"
 
         }
 
         object Ktx {
 
-            // Release: https://developer.android.com/jetpack/androidx/releases/core
+            // Releases: https://developer.android.com/jetpack/androidx/releases/core
             const val CORE = "1.1.0"
 
         }
@@ -74,7 +74,7 @@ object Versions {
 
                 object Lifecycle {
 
-                    // Release: https://developer.android.com/jetpack/androidx/releases/lifecycle
+                    // Releases: https://developer.android.com/jetpack/androidx/releases/lifecycle
                     const val EXTENSIONS = "2.1.0"
 
                 }
@@ -83,7 +83,7 @@ object Versions {
 
             object Test {
 
-                // Release: https://developer.android.com/jetpack/androidx/releases/arch
+                // Releases: https://developer.android.com/jetpack/androidx/releases/arch
                 const val CORE_TESTING = "2.1.0"
 
             }
@@ -92,16 +92,16 @@ object Versions {
 
         object Test {
 
-            // Release: https://developer.android.com/jetpack/androidx/releases/test
+            // Releases: https://developer.android.com/jetpack/androidx/releases/test
             const val CORE = "1.2.0"
 
-            // Release: https://developer.android.com/jetpack/androidx/releases/test
+            // Releases: https://developer.android.com/jetpack/androidx/releases/test
             const val J_UNIT = "1.1.1"
 
-            // Release: https://developer.android.com/jetpack/androidx/releases/test
+            // Releases: https://developer.android.com/jetpack/androidx/releases/test
             const val ESPRESSO = "3.2.0"
 
-            // Release: https://github.com/robolectric/robolectric/releases
+            // Releases: https://github.com/robolectric/robolectric/releases
             const val ROBOLECTRIC = "4.3.1"
 
         }
@@ -113,7 +113,7 @@ object Versions {
         @Suppress("MemberNameEqualsClassName")
         object Koin {
 
-            // Release: https://github.com/InsertKoinIO/koin/releases
+            // Releases: https://github.com/InsertKoinIO/koin/releases
             const val KOIN = "2.0.1"
 
         }
@@ -124,14 +124,14 @@ object Versions {
 
         object OkHttp {
 
-            // Release: https://github.com/square/okhttp/releases
+            // Releases: https://github.com/square/okhttp/releases
             const val OK_HTTP = "4.2.2"
 
         }
 
         object Rest {
 
-            // Release: https://github.com/square/retrofit/releases
+            // Releases: https://github.com/square/retrofit/releases
             const val RETROFIT = "2.6.3"
 
         }
@@ -143,7 +143,7 @@ object Versions {
         @Suppress("MemberNameEqualsClassName")
         object Glide {
 
-            // Release: https://github.com/bumptech/glide/releases
+            // Releases: https://github.com/bumptech/glide/releases
             const val GLIDE = "4.10.0"
 
         }
@@ -152,14 +152,14 @@ object Versions {
 
     object Util {
 
-        // Release: https://mvnrepository.com/artifact/com.whiteelephant/monthandyearpicker
+        // Releases: https://mvnrepository.com/artifact/com.whiteelephant/monthandyearpicker
         const val MONTH_YEAR_PICKER = "1.3.0"
 
     }
 
     object Log {
 
-        // Release: https://github.com/JakeWharton/timber/releases
+        // Releases: https://github.com/JakeWharton/timber/releases
         const val TIMBER = "4.7.1"
 
     }
@@ -171,31 +171,31 @@ object Versions {
         @Suppress("MemberNameEqualsClassName")
         object Spek {
 
-            // Release: https://github.com/spekframework/spek/releases
+            // Releases: https://github.com/spekframework/spek/releases
             const val SPEK = "2.0.9"
 
         }
 
         object JUnit {
 
-            // Release: https://github.com/junit-team/junit4/releases
+            // Releases: https://github.com/junit-team/junit4/releases
             const val J_UNIT_4 = "4.12"
 
-            // Release: https://github.com/junit-team/junit5/releases
+            // Releases: https://github.com/junit-team/junit5/releases
             const val J_UNIT_5 = "5.6.1"
 
         }
 
         object Assert {
 
-            // Release: https://github.com/robfletcher/strikt/releases
+            // Releases: https://github.com/robfletcher/strikt/releases
             const val STRIKT = "0.23.2"
 
         }
 
         object Mock {
 
-            // Release: https://github.com/mockk/mockk/releases
+            // Releases: https://github.com/mockk/mockk/releases
             const val MOCK_K = "1.9.3"
 
         }
