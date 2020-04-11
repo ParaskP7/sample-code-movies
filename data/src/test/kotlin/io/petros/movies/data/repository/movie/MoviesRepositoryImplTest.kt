de9de82df819b8366a858d997fb2059ca4c8bda8
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-04-11 14:39:00 +0300

    [movies-18] test: Improve movies repository test
    This is done so that the later on jacoco reports will end up covering
    the whole class. The way this test is implemented now is not covering
    the class to 100%.

--- data/src/test/kotlin/io/petros/movies/data/repository/movie/MoviesRepositoryImplTest.kt
+++ data/src/test/kotlin/io/petros/movies/data/repository/movie/MoviesRepositoryImplTest.kt
@@ -1,30 +1,38 @@
 package io.petros.movies.data.repository.movie
 
-import io.mockk.coVerify
+import io.mockk.coEvery
 import io.mockk.mockk
+import io.petros.movies.domain.model.Result
 import io.petros.movies.network.WebService
 import io.petros.movies.test.domain.MOVIE_MONTH
 import io.petros.movies.test.domain.MOVIE_YEAR
 import io.petros.movies.test.domain.NEXT_PAGE
+import io.petros.movies.test.domain.moviesPage
 import io.petros.movies.test.utils.MainCoroutineScopeRule
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Rule
 import org.junit.Test
+import strikt.api.expect
+import strikt.assertions.isEqualTo
 
 @ExperimentalCoroutinesApi
 class MoviesRepositoryImplTest {
 
     @get:Rule val coroutineScope = MainCoroutineScopeRule()
 
+    private val moviesPage = Result.Success(moviesPage())
+
     private val webServiceMock = mockk<WebService>()
     private val testedClass = MoviesRepositoryImpl(webServiceMock)
 
     @Test
-    fun `when load movies is triggered, then web service triggers load movies`() = coroutineScope.runBlockingTest {
-        testedClass.loadMovies(MOVIE_YEAR, MOVIE_MONTH, NEXT_PAGE)
+    fun `when load movies is triggered, then the movies page is the expected one`() = coroutineScope.runBlockingTest {
+        coEvery { webServiceMock.loadMovies(MOVIE_YEAR, MOVIE_MONTH, NEXT_PAGE) } returns moviesPage.value
 
-        coVerify { webServiceMock.loadMovies(MOVIE_YEAR, MOVIE_MONTH, NEXT_PAGE) }
+        val result = testedClass.loadMovies(MOVIE_YEAR, MOVIE_MONTH, NEXT_PAGE)
+
+        expect { that(result).isEqualTo(moviesPage) }
     }
 
 }
