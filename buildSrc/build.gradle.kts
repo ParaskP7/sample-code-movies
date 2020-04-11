e629bd332705b7382f0bfd48c1a529bfa37128dc
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-04-11 15:28:14 +0300

    [movies-18] feature: Register coverage plugin
    This is so that it becomes available and modules can then add it to
    their build plugin configuration.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -4,11 +4,21 @@
 
 plugins {
     `kotlin-dsl`
+    `java-gradle-plugin`
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+gradlePlugin {
+    plugins {
+        register("io.petros.movies.coverage") {
+            id = "io.petros.movies.coverage"
+            implementationClass = "io.petros.movies.plugin.coverage.CoveragePlugin"
+        }
+    }
 }
 
 /* REPOS */
