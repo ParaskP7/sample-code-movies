adeff4e00af256bd345eab7f84c25b4a68d20c0e
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-04-14 14:40:28 +0300

    [movies-19] refactor: Move spek below junit test dependencies
    This reordering was done in order to preserve consistency across
    build configurations.

--- buildSrc/src/main/kotlin/io/petros/movies/config/deps/Deps.kt
+++ buildSrc/src/main/kotlin/io/petros/movies/config/deps/Deps.kt
@@ -230,17 +230,17 @@ object Deps {
 
     object Test {
 
-        object Spek {
-
-            val DSL = "org.spekframework.spek2:spek-dsl-jvm:${Versions.Test.Spek.SPEK}"
-            val J_UNIT_5 = "org.spekframework.spek2:spek-runner-junit5:${Versions.Test.Spek.SPEK}"
-
-        }
-
         object JUnit {
 
             val J_UNIT_4 = "junit:junit:${Versions.Test.JUnit.J_UNIT_4}"
             val J_UNIT_5 = "org.junit.vintage:junit-vintage-engine:${Versions.Test.JUnit.J_UNIT_5}"
+
+        }
+
+        object Spek {
+
+            val DSL = "org.spekframework.spek2:spek-dsl-jvm:${Versions.Test.Spek.SPEK}"
+            val J_UNIT_5 = "org.spekframework.spek2:spek-runner-junit5:${Versions.Test.Spek.SPEK}"
 
         }
 
