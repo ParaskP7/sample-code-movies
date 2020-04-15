34b6bb4d3eef97936bf0bdf786ea50b8636f954c
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-04-15 14:48:16 +0300

    [movies-20] test: Add unit and spek tests for kotlin extensions file

---
+++ utils/src/spekTest/kotlin/io/petros/movies/utils/KotlinExtensionsKtSpek.kt
@@ -0,0 +1,156 @@
+package io.petros.movies.utils
+
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.gherkin.Feature
+import strikt.api.expect
+import strikt.assertions.isA
+import strikt.assertions.isEqualTo
+import java.text.ParseException
+import java.util.*
+
+class KotlinExtensionsKtSpek : Spek({
+
+    Feature("Single characters") {
+        Scenario("empty") {
+            var result: String? = null
+            When("empty is triggered") {
+                result = empty()
+            }
+            Then("an empty string is returned") {
+                expect { that(result).isEqualTo("") }
+            }
+        }
+        Scenario("space") {
+            var result: String? = null
+            When("space is triggered") {
+                result = space()
+            }
+            Then("a space string is returned") {
+                expect { that(result).isEqualTo(" ") }
+            }
+        }
+        Scenario("left parentheses") {
+            var result: String? = null
+            When("left parentheses is triggered") {
+                result = leftParentheses()
+            }
+            Then("a left parentheses string is returned") {
+                expect { that(result).isEqualTo("(") }
+            }
+        }
+        Scenario("right parentheses") {
+            var result: String? = null
+            When("right parentheses is triggered") {
+                result = rightParentheses()
+            }
+            Then("a right parentheses string is returned") {
+                expect { that(result).isEqualTo(")") }
+            }
+        }
+        Scenario("star") {
+            var result: String? = null
+            When("star is triggered") {
+                result = star()
+            }
+            Then("a star string is returned") {
+                expect { that(result).isEqualTo("★") }
+            }
+        }
+        Scenario("colon") {
+            var result: String? = null
+            When("colon is triggered") {
+                result = colon()
+            }
+            Then("a colon string is returned") {
+                expect { that(result).isEqualTo(":") }
+            }
+        }
+    }
+
+    Feature("Parentheses") {
+        Scenario("string") {
+            var string: String? = null
+            var result: String? = null
+            Given("string") {
+                string = "word"
+            }
+            When("with parentheses is triggered") {
+                result = string?.withParentheses()
+            }
+            Then("the string is enclosed in parentheses") {
+                expect { that(result).isEqualTo("($string)") }
+            }
+        }
+        Scenario("integer") {
+            var integer: Int? = null
+            var result: String? = null
+            Given("integer") {
+                integer = 1
+            }
+            When("with parentheses is triggered") {
+                result = integer?.withParentheses()
+            }
+            Then("the integer is enclosed in parentheses") {
+                expect { that(result).isEqualTo("($integer)") }
+            }
+        }
+    }
+
+    Feature("date") {
+        Scenario("valid to date") {
+            var date: String? = null
+            var result: Date? = null
+            Given("valid date as string") {
+                date = "2019-09-17"
+            }
+            When("to date is triggered") {
+                result = date?.toDate(MOVIE_DATE_FORMAT)
+            }
+            Then("the expect date is returned") {
+                expect { that(result).isEqualTo(GregorianCalendar(2019, Calendar.SEPTEMBER, 17).time) }
+            }
+        }
+        Scenario("invalid to date") {
+            var date: String? = null
+            Given("invalid date as string") {
+                date = "09-17"
+            }
+            When("to date is triggered, then throw parse exception") {
+                try {
+                    date?.toDate(MOVIE_DATE_FORMAT)
+                } catch (e: Exception) {
+                    expect { that(e).isA<ParseException>() }
+                }
+            }
+        }
+        Scenario("to year") {
+            val year = 2019
+            var date: Date? = null
+            var result: Int? = null
+            Given("date") {
+                date = GregorianCalendar(year, Calendar.SEPTEMBER, 17).time
+            }
+            When("to year is triggered") {
+                result = date?.toYear()
+            }
+            Then("the expect year is returned") {
+                expect { that(result).isEqualTo(year) }
+            }
+        }
+        Scenario("to month") {
+            val month = MonthOfYear.SEPTEMBER
+            var date: Date? = null
+            var result: String? = null
+            Given("date") {
+                date = GregorianCalendar(2019, month.number, 17).time
+            }
+            When("to month is triggered") {
+                result = date?.toMonth()
+            }
+            Then("the expect month is returned") {
+                expect { that(result).isEqualTo(month.label) }
+            }
+        }
+    }
+
+})
---
+++ utils/src/test/kotlin/io/petros/movies/utils/KotlinExtensionsKtTest.kt
@@ -0,0 +1,101 @@
+package io.petros.movies.utils
+
+import org.junit.Test
+import strikt.api.expect
+import strikt.assertions.isEqualTo
+import java.text.ParseException
+import java.util.*
+
+class KotlinExtensionsKtTest {
+
+    /* SINGLE CHARACTER */
+
+    @Test
+    fun `when empty is triggered, then an empty string is returned`() {
+        expect { that(empty()).isEqualTo("") }
+    }
+
+    @Test
+    fun `when space is triggered, then a space string is returned`() {
+        expect { that(space()).isEqualTo(" ") }
+    }
+
+    @Test
+    fun `when left parentheses is triggered, then a left parenthesis string is returned`() {
+        expect { that(leftParentheses()).isEqualTo("(") }
+    }
+
+    @Test
+    fun `when right parentheses is triggered, then a right parenthesis string is returned`() {
+        expect { that(rightParentheses()).isEqualTo(")") }
+    }
+
+    @Test
+    fun `when start is triggered, then a star string is returned`() {
+        expect { that(star()).isEqualTo("★") }
+    }
+
+    @Test
+    fun `when colon is triggered, then a colon string is returned`() {
+        expect { that(colon()).isEqualTo(":") }
+    }
+
+    @Test
+    fun `given string, when with parentheses is triggered, then the string is enclosed in parentheses`() {
+        val string = "word"
+
+        val result = string.withParentheses()
+
+        expect { that(result).isEqualTo("($string)") }
+    }
+
+    /* PARENTHESES */
+
+    @Test
+    fun `given integer, when with parentheses is triggered, then the integer is enclosed in parentheses`() {
+        val integer = 1
+
+        val result = integer.withParentheses()
+
+        expect { that(result).isEqualTo("($integer)") }
+    }
+
+    /* DATE */
+
+    @Test
+    fun `given valid date as string, when to date is triggered, then the expect date is returned`() {
+        val date = "2019-09-17"
+
+        val result = date.toDate(MOVIE_DATE_FORMAT)
+
+        expect { that(result).isEqualTo(GregorianCalendar(2019, Calendar.SEPTEMBER, 17).time) }
+    }
+
+    @Test(expected = ParseException::class)
+    fun `given invalid date as string, when to date is triggered, then throw parse exception`() {
+        val date = "09-17"
+
+        date.toDate(MOVIE_DATE_FORMAT)
+    }
+
+    @Test
+    fun `when to year is triggered, then the expect year is returned`() {
+        val year = 2019
+        val date = GregorianCalendar(year, Calendar.SEPTEMBER, 17).time
+
+        val result = date.toYear()
+
+        expect { that(result).isEqualTo(year) }
+    }
+
+    @Test
+    fun `when to month is triggered, then the expect month is returned`() {
+        val month = MonthOfYear.SEPTEMBER
+        val date = GregorianCalendar(2019, month.number, 17).time
+
+        val result = date.toMonth()
+
+        expect { that(result).isEqualTo(month.label) }
+    }
+
+}
