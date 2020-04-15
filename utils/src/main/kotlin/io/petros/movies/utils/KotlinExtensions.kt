f22f770fe43399c90362a88b7a8cddb9eb9e9ecf
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-04-15 14:52:20 +0300

    [movies-20] refactor: Move tricks group to its own kotlin tricks file

--- utils/src/main/kotlin/io/petros/movies/utils/KotlinExtensions.kt
+++ utils/src/main/kotlin/io/petros/movies/utils/KotlinExtensions.kt
@@ -57,13 +57,3 @@ fun Date.toMonth(): String {
     val monthDate = SimpleDateFormat(SIMPLE_DATE_FORMAT_PATTERN, Locale.US)
     return monthDate.format(calendar.time)
 }
-
-/* TRICKS */
-
-/**
- * The superpower of sealed classes is only unleashed if when is used as an expression. Otherwise, the compiler doesn’t
- * force developers to handle all cases. However, with this little exhaustive trick, a when statement can be enforce to
- * become exhaustive.
- */
-val <T> T.exhaustive: T
-    get() = this
