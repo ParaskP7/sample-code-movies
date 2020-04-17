4efa87f091331c0994355b15f834c2315877562d
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-04-17 17:09:33 +0300

    [movies-21] test: Add missing robolectric tests for movies adapter

--- feature/movies/src/robolectricTest/kotlin/io/petros/movies/movies/list/MoviesAdapterRobolectricTest.kt
+++ feature/movies/src/robolectricTest/kotlin/io/petros/movies/movies/list/MoviesAdapterRobolectricTest.kt
@@ -16,6 +16,8 @@ import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import strikt.api.expect
 import strikt.assertions.isEqualTo
+import strikt.assertions.isFalse
+import strikt.assertions.isTrue
 
 @RunWith(RobolectricTestRunner::class)
 class MoviesAdapterRobolectricTest {
@@ -35,11 +37,54 @@ class MoviesAdapterRobolectricTest {
         expect { that(testedClass.status).isEqualTo(AdapterStatus.IDLE) }
     }
 
+    @Test
+    fun `when adapter is set, then is loading is false`() {
+        expect { that(testedClass.isLoading()).isFalse() }
+    }
+
+    /* IS LOADING */
+
+    @Test
+    fun `given status change to idle, when is loading is queried, then is loading is false`() {
+        testedClass.status = AdapterStatus.IDLE
+
+        expect { that(testedClass.isLoading()).isFalse() }
+    }
+
+    @Test
+    fun `given status change to loading, when is loading is queried, then is loading is true`() {
+        testedClass.status = AdapterStatus.LOADING
+
+        expect { that(testedClass.isLoading()).isTrue() }
+    }
+
+    @Test
+    fun `given status change to error, when is loading is queried, then is loading is false`() {
+        testedClass.status = AdapterStatus.ERROR
+
+        expect { that(testedClass.isLoading()).isFalse() }
+    }
+
     /* ITEMS */
 
     @Test
     fun `when adapter is set, then all items are used`() {
         expect { that(testedClass.itemCount).isEqualTo(items.size) }
+    }
+
+    /* CONTEXT */
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `given not attached to recycle view, when creating a view holder, then throw illegal argument exception`() {
+        testedClass.onCreateViewHolder(mockk(), VIEW_TYPE_MOVIE)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun `given detached from recycle view, when creating a view holder, then throw illegal argument exception`() {
+        testedClass.onAttachedToRecyclerView(recyclerView)
+        testedClass.onDetachedFromRecyclerView(recyclerView)
+
+        testedClass.onCreateViewHolder(mockk(), VIEW_TYPE_MOVIE)
     }
 
     /* VIEW HOLDER */
@@ -71,12 +116,19 @@ class MoviesAdapterRobolectricTest {
         expect { that(viewHolder.javaClass.name).isEqualTo(ErrorViewHolder::class.java.name) }
     }
 
+    @Test(expected = IllegalArgumentException::class)
+    fun `given an unknown view type, when creating a view holder, then throw illegal argument exception`() {
+        testedClass.onAttachedToRecyclerView(recyclerView)
+
+        testedClass.onCreateViewHolder(mockk(), 2)
+    }
+
     @Test
     fun `given a movie view type, when binding a view holder, then a movie item is bind`() {
         testedClass.onAttachedToRecyclerView(recyclerView)
         testedClass.onCreateViewHolder(mockk(), VIEW_TYPE_MOVIE)
         testedClass.status = AdapterStatus.IDLE
-        val position = 1
+        val position = 0
         val viewHolderMock = mockk<MovieViewHolder>()
 
         testedClass.onBindViewHolder(viewHolderMock, position)
@@ -85,10 +137,36 @@ class MoviesAdapterRobolectricTest {
     }
 
     @Test
-    fun `given a progress view type, when binding a view holder, then a movie item is not bind`() {
+    fun `given a movie view type at last position, when binding a view holder, then a movie item is bind`() {
+        testedClass.onAttachedToRecyclerView(recyclerView)
+        testedClass.onCreateViewHolder(mockk(), VIEW_TYPE_MOVIE)
+        testedClass.status = AdapterStatus.IDLE
+        val position = items.size - 1
+        val viewHolderMock = mockk<MovieViewHolder>()
+
+        testedClass.onBindViewHolder(viewHolderMock, position)
+
+        verify { viewHolderMock.bind(items[position]) }
+    }
+
+    @Test
+    fun `given a progress view type at last position, when binding a view holder, then a movie item is not bind`() {
         testedClass.onAttachedToRecyclerView(recyclerView)
         testedClass.onCreateViewHolder(mockk(), VIEW_TYPE_PROGRESS)
         testedClass.status = AdapterStatus.LOADING
+        val position = items.size
+        val viewHolderMock = mockk<MovieViewHolder>()
+
+        testedClass.onBindViewHolder(viewHolderMock, position)
+
+        verify(exactly = 0) { viewHolderMock.bind(any()) }
+    }
+
+    @Test
+    fun `given an error view type at last position, when binding a view holder, then a movie item is not bind`() {
+        testedClass.onAttachedToRecyclerView(recyclerView)
+        testedClass.onCreateViewHolder(mockk(), VIEW_TYPE_ERROR)
+        testedClass.status = AdapterStatus.ERROR
         val position = items.size
         val viewHolderMock = mockk<MovieViewHolder>()
 
