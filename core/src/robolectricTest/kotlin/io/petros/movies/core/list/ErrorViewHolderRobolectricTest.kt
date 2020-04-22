6c41a5bf2a80e57bf0b4c45de4a32c2973cb7404
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-04-22 13:22:50 +0300

    [movies-21] test: Replace mock with stub testing for view holder tests
    Instead of mocking the view and verify that setting the on click
    listener is called during initialization it is better if Robolectric's
    full power is utilised to test if after having the on click listener
    being set that the views are having an on click listener.

--- core/src/robolectricTest/kotlin/io/petros/movies/core/list/ErrorViewHolderRobolectricTest.kt
+++ core/src/robolectricTest/kotlin/io/petros/movies/core/list/ErrorViewHolderRobolectricTest.kt
@@ -1,22 +1,23 @@
 package io.petros.movies.core.list
 
 import android.view.View
-import io.mockk.mockk
-import io.mockk.verify
+import io.petros.movies.android_test.context.TestContextProvider.context
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import strikt.api.expect
+import strikt.assertions.isTrue
 
 @RunWith(RobolectricTestRunner::class)
 class ErrorViewHolderRobolectricTest {
 
     @Test
     fun `when the error view holder is instantiated, then a click listener is set on the item view`() {
-        val itemViewMock = mockk<View>()
+        val itemView = View(context())
 
-        ErrorViewHolder(itemViewMock) { }
+        ErrorViewHolder(itemView) { }
 
-        verify { itemViewMock.setOnClickListener(any()) }
+        expect { that(itemView.hasOnClickListeners()).isTrue() }
     }
 
 }
--- core/src/robolectricTest/kotlin/io/petros/movies/core/list/ProgressViewHolderRobolectricTest.kt
+++ core/src/robolectricTest/kotlin/io/petros/movies/core/list/ProgressViewHolderRobolectricTest.kt
@@ -1,22 +1,23 @@
 package io.petros.movies.core.list
 
 import android.view.View
-import io.mockk.mockk
-import io.mockk.verify
+import io.petros.movies.android_test.context.TestContextProvider
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import strikt.api.expect
+import strikt.assertions.isFalse
 
 @RunWith(RobolectricTestRunner::class)
 class ProgressViewHolderRobolectricTest {
 
     @Test
     fun `when the progress view holder is instantiated, then no click listener is set on the item view`() {
-        val itemViewMock = mockk<View>()
+        val itemView = View(TestContextProvider.context())
 
-        ProgressViewHolder(itemViewMock)
+        ProgressViewHolder(itemView)
 
-        verify(exactly = 0) { itemViewMock.setOnClickListener(any()) }
+        expect { that(itemView.hasOnClickListeners()).isFalse() }
     }
 
 }
