c7acba824bfd9dc4f3cf975cfd3f17ab31bb59a0
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-05-05 15:20:13 +0300

    [movies-23] feature: Replace image with shapeable image view for glide

--- core/src/main/kotlin/io/petros/movies/core/image/glide/GlideExtensions.kt
+++ core/src/main/kotlin/io/petros/movies/core/image/glide/GlideExtensions.kt
@@ -1,12 +1,12 @@
 package io.petros.movies.core.image.glide
 
-import android.widget.ImageView
 import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
+import com.google.android.material.imageview.ShapeableImageView
 
 /* IMAGE VIEW */
 
-fun ImageView.displayImage(url: String?, placeholder: Int = android.R.color.darker_gray) {
+fun ShapeableImageView.displayImage(url: String?, placeholder: Int = android.R.color.darker_gray) {
     if (url != null) {
         Glide.with(this)
             .load(url)
