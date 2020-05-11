8e1f391edf5ebaf2ea3172b26639c615e7c74e2b
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-05-11 13:24:37 +0300

    [movies] update: Update coroutines to latest 1.3.6 version

--- buildSrc/src/main/kotlin/io/petros/movies/config/deps/Versions.kt
+++ buildSrc/src/main/kotlin/io/petros/movies/config/deps/Versions.kt
@@ -34,7 +34,7 @@ object Versions {
     object Kotlin {
 
         // Releases: https://github.com/Kotlin/kotlinx.coroutines/releases
-        const val COROUTINES = "1.3.5" // Released: 17-03-20
+        const val COROUTINES = "1.3.6" // Released: 08-05-20
 
     }
 
