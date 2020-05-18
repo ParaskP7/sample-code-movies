614be50373a48003e2712b235eb4cbed207c2dc4
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-05-18 19:19:48 +0300

    [movies] feature: Add base fragment
    This step is done as a preparation step for the 'Navigation Component'
    functionality.

---
+++ core/src/main/kotlin/io/petros/movies/core/fragment/BaseFragment.kt
@@ -0,0 +1,69 @@
+package io.petros.movies.core.fragment
+
+import android.content.res.Configuration
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import timber.log.Timber
+
+@Suppress("TooManyFunctions")
+abstract class BaseFragment(layout: Int) : Fragment(layout) {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        Timber.v("${javaClass.simpleName} create view. [Container: $container, Bundle: $savedInstanceState]")
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Timber.v("${javaClass.simpleName} create view. [View: $view, Bundle: $savedInstanceState]")
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Timber.v("${javaClass.simpleName} started.")
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        Timber.v("${javaClass.simpleName} view state restored. [Bundle: $savedInstanceState]")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.v("${javaClass.simpleName} resumed.")
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        Timber.v("${javaClass.simpleName} new config. [Config: $newConfig]")
+    }
+
+    override fun onPause() {
+        Timber.v("${javaClass.simpleName} paused.")
+        super.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Timber.v("${javaClass.simpleName} instance state saved. [Bundle: %s]", outState)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        Timber.v("${javaClass.simpleName} stopped.")
+        super.onStop()
+    }
+
+    override fun onDestroyView() {
+        Timber.v("${javaClass.simpleName} destroy view.")
+        super.onDestroyView()
+    }
+
+    override fun onDestroy() {
+        Timber.v("${javaClass.simpleName} destroyed.")
+        super.onDestroy()
+    }
+
+}
