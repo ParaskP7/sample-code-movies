5de42c1594b10a8a841cc2935d3988509090d090
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-05-18 19:20:26 +0300

    [movies] feature: Add mvi fragment
    This step is done as a preparation step for the 'Navigation Component'
    functionality.

---
+++ core/src/main/kotlin/io/petros/movies/core/fragment/MviFragment.kt
@@ -0,0 +1,40 @@
+package io.petros.movies.core.fragment
+
+import androidx.lifecycle.Observer
+import io.petros.movies.core.view_model.MviViewModel
+import timber.log.Timber
+
+abstract class MviFragment<
+        INTENT : Any,
+        STATE : Any,
+        SIDE_EFFECT : Any,
+        VIEW_MODEL : MviViewModel<INTENT, STATE, SIDE_EFFECT>
+        >(layout: Int) : BaseFragment(layout) {
+
+    abstract val viewModel: VIEW_MODEL
+
+    private val state = Observer<STATE> {
+        Timber.v("${javaClass.simpleName} observed state. [State: $it]")
+        renderState(it)
+    }
+
+    private val sideEffect = Observer<SIDE_EFFECT> {
+        Timber.v("${javaClass.simpleName} observed side effect. [Side Effect: $it]")
+        renderSideEffect(it)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initObservers()
+    }
+
+    private fun initObservers() {
+        viewModel.state().observe(this, state)
+        viewModel.sideEffect().observe(this, sideEffect)
+    }
+
+    abstract fun renderState(state: STATE)
+
+    abstract fun renderSideEffect(sideEffect: SIDE_EFFECT): Unit?
+
+}
