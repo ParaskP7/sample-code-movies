1e2cb1494bbdd8900b6aea3f4cc063550a3ba6b4
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-05-21 16:51:42 +0300

    [movies-25] feature: Update movie details fragment to load a movie by id

--- app/src/main/kotlin/io/petros/movies/app/di/koin/AppModule.kt
+++ app/src/main/kotlin/io/petros/movies/app/di/koin/AppModule.kt
@@ -1,7 +1,8 @@
 package io.petros.movies.app.di.koin
 
+import io.petros.movies.movie_details.di.koin.movieDetailsModule
 import io.petros.movies.movies.di.koin.moviesModule
 
-private fun featureModules() = moviesModule()
+private fun featureModules() = moviesModule() + movieDetailsModule()
 
 fun appModule() = featureModules()
--- feature/movie-details/src/main/kotlin/io/petros/movies/movie_details/MovieDetailsFragment.kt
+++ feature/movie-details/src/main/kotlin/io/petros/movies/movie_details/MovieDetailsFragment.kt
@@ -1,30 +1,86 @@
 package io.petros.movies.movie_details
 
-import android.os.Bundle
-import android.view.View
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.navArgs
-import io.petros.movies.core.fragment.BaseFragment
+import com.google.android.material.snackbar.Snackbar
+import io.petros.movies.core.fragment.MviFragment
 import io.petros.movies.core.image.glide.displayImage
 import io.petros.movies.core.view_binding.viewBinding
-import io.petros.movies.domain.model.movie.Movie
 import io.petros.movies.movie_details.databinding.MovieDetailsFragmentBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MovieDetailsFragment : BaseFragment(R.layout.movie_details_fragment) {
+@Suppress("ForbiddenComment")
+class MovieDetailsFragment : MviFragment<
+        MovieDetailsIntent,
+        MovieDetailsState,
+        MovieDetailsSideEffect,
+        MovieDetailsViewModel>(R.layout.movie_details_fragment) {
 
     private val binding by viewBinding(MovieDetailsFragmentBinding::bind)
     private val safeArgs: MovieDetailsFragmentArgs by navArgs()
+    override val viewModel: MovieDetailsViewModel by viewModel()
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        safeArgs.movie?.let { initMovie(it) }
+    private var snackbar: Snackbar? = null
+
+    override fun onDestroyView() {
+        snackbar = null
+        super.onDestroyView()
     }
 
-    private fun initMovie(movie: Movie) {
-        binding.ivBackdrop.displayImage(movie.backdrop)
-        binding.tvTitle.text = movie.title
-        binding.tvReleaseDate.text = movie.releaseDate()
-        binding.tvVote.text = movie.vote()
-        binding.tvOverview.text = movie.overview
+    /* STATE */
+
+    override fun renderState(state: MovieDetailsState) = when (state.status) {
+        is MovieDetailsStatus.Init -> renderInitState()
+        is MovieDetailsStatus.Loading -> renderLoadingState()
+        is MovieDetailsStatus.Loaded -> renderLoadedState(state)
+    }
+
+    private fun renderInitState() {
+        safeArgs.movie.id.let {
+            viewModel.process(
+                MovieDetailsIntent.LoadMovie(
+                    id = it
+                )
+            )
+        }
+    }
+
+    private fun renderLoadingState() {
+        binding.pbLoading.isVisible = true
+        binding.ctrLoaded.isVisible = false
+    }
+
+    private fun renderLoadedState(state: MovieDetailsState) {
+        binding.pbLoading.isVisible = false
+        binding.ctrLoaded.isVisible = true
+        state.movie?.let {
+            binding.ivBackdrop.displayImage(state.movie.backdrop)
+            binding.tvTitle.text = state.movie.title
+            binding.tvReleaseDate.text = state.movie.releaseDate()
+            binding.tvVote.text = state.movie.vote()
+            binding.tvOverview.text = state.movie.overview
+        }
+    }
+
+    /* SIDE EFFECT */
+
+    override fun renderSideEffect(sideEffect: MovieDetailsSideEffect) = when (sideEffect) {
+        is MovieDetailsSideEffect.Error -> renderErrorSideEffect()
+    }
+
+    private fun renderErrorSideEffect() {
+        snackbar = Snackbar
+            .make(binding.ctrMovieDetails, R.string.sbLoadMovieError, Snackbar.LENGTH_INDEFINITE)
+            .setAction(R.string.sbLoadMovieErrorAction) {
+                safeArgs.movie.id.let {
+                    viewModel.process(
+                        MovieDetailsIntent.LoadMovie(
+                            id = it
+                        )
+                    )
+                }
+            }
+        snackbar?.show()
     }
 
 }
