f70b3c740fd914445f02455281e5899cbf508346
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-05-21 16:36:49 +0300

    [movies] test: Update movies repository impl spek to match the test one

--- data/src/spekTest/kotlin/io/petros/movies/data/repository/movie/MoviesRepositoryImplSpek.kt
+++ data/src/spekTest/kotlin/io/petros/movies/data/repository/movie/MoviesRepositoryImplSpek.kt
@@ -1,26 +1,37 @@
 package io.petros.movies.data.repository.movie
 
-import io.mockk.coVerify
+import io.mockk.coEvery
 import io.mockk.mockk
+import io.petros.movies.domain.model.Result
+import io.petros.movies.domain.model.movie.MoviesPage
 import io.petros.movies.network.WebService
+import io.petros.movies.test.domain.moviesPage
 import io.petros.movies.test.utils.CoroutineSpek
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import org.spekframework.spek2.style.gherkin.Feature
+import strikt.api.expect
+import strikt.assertions.isEqualTo
 
 @ExperimentalCoroutinesApi
 class MoviesRepositoryImplSpek : CoroutineSpek({
+
+    val moviesPage = Result.Success(moviesPage())
 
     val webServiceMock = mockk<WebService>()
 
     Feature("Movies repository") {
         val testedClass by memoized { MoviesRepositoryImpl(webServiceMock) }
         Scenario("loading movies") {
-            When("load movies is triggered") {
-                runBlocking { testedClass.loadMovies(MOVIE_YEAR, MOVIE_MONTH, SECOND_PAGE) }
+            var result: Result<MoviesPage>? = null
+            Given("movies page response") {
+                coEvery { webServiceMock.loadMovies(MOVIE_YEAR, MOVIE_MONTH, SECOND_PAGE) } returns moviesPage.value
             }
-            Then("web service triggers load movies") {
-                coVerify { webServiceMock.loadMovies(MOVIE_YEAR, MOVIE_MONTH, SECOND_PAGE) }
+            When("load movies is triggered") {
+                runBlocking { result = testedClass.loadMovies(MOVIE_YEAR, MOVIE_MONTH, SECOND_PAGE) }
+            }
+            Then("the movies page is the expected one") {
+                expect { that(result).isEqualTo(moviesPage) }
             }
         }
     }
