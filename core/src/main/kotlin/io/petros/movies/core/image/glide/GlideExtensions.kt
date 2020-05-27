37e75f10d4629b6a6407eba311641417657f9da1
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-05-27 16:43:58 +0300

    [movies] refactor: Remove unnecessary if/else glide extension logic
    The '.placeholder(...)' builder logic already does this job.

--- core/src/main/kotlin/io/petros/movies/core/image/glide/GlideExtensions.kt
+++ core/src/main/kotlin/io/petros/movies/core/image/glide/GlideExtensions.kt
@@ -1,19 +1,14 @@
 package io.petros.movies.core.image.glide
 
-import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.google.android.material.imageview.ShapeableImageView
 
 /* IMAGE VIEW */
 
 fun ShapeableImageView.displayImage(url: String?, placeholder: Int = android.R.color.darker_gray) {
-    if (url != null) {
-        Glide.with(this)
-            .load(url)
-            .placeholder(placeholder)
-            .fitCenter()
-            .into(this)
-    } else {
-        setImageDrawable(ContextCompat.getDrawable(context, placeholder))
-    }
+    Glide.with(this)
+        .load(url)
+        .placeholder(placeholder)
+        .fitCenter()
+        .into(this)
 }
