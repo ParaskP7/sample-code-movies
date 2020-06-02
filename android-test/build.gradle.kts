541c93f7b6219f8b437147d171955a1c390a862c
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-06-02 16:26:00 +0300

    [movies] build: Configure android test based on deps analysis advise

--- android-test/build.gradle.kts
+++ android-test/build.gradle.kts
@@ -1,6 +1,7 @@
 @file:Suppress("InvalidPackageDeclaration")
 
 import io.petros.movies.config.deps.Deps
+import io.petros.movies.config.deps.identifier
 
 plugins {
     id(Plugins.Id.Android.LIBRARY)
@@ -9,20 +10,41 @@ plugins {
     id(Plugins.Id.Dependency.VERSIONS)
 }
 
-dependencies {
-    implementation(project(Deps.Project.TestImplementation.Kotlin.TEST))
+dependencyAnalysis {
+    issues {
+        onUnusedDependencies {
+            exclude(
+                Deps.Material.MATERIAL.identifier() // Ignore remove advise. Required for theme purposes.
+            )
+        }
+        onUsedTransitiveDependencies {
+            exclude(
+                Deps.Test.Hamcrest.CORE.identifier(), // Ignore add to 'api' advice. Otherwise, Espresso fails.
+                Deps.Test.Hamcrest.LIBRARY.identifier() // Ignore add to 'implementation' advice. Otherwise, Espresso fails.
+            )
+        }
+        onIncorrectConfiguration {
+            exclude(
+                Deps.Kotlin.Coroutines.Test.TEST.identifier(), // Ignore change to 'api' advice.
+                Deps.Test.Spek.DSL.identifier() // Ignore change to 'api' advice.
+            )
+        }
+    }
+}
 
+dependencies {
     implementation(Deps.Kotlin.Core.KOTLIN)
+    implementation(Deps.Kotlin.Coroutines.CORE)
     implementation(Deps.Kotlin.Coroutines.Test.TEST)
     implementation(Deps.Material.MATERIAL)
     implementation(Deps.Android.Core.RECYCLER_VIEW)
+    implementation(Deps.Android.Arch.Core.RUNTIME)
     implementation(Deps.Android.Test.CORE)
+    implementation(Deps.Android.Test.Espresso.CORE)
     implementation(Deps.Log.TIMBER)
     implementation(Deps.Test.Spek.DSL)
     implementation(Deps.Test.Spek.J_UNIT_5)
     runtimeOnly(Deps.Kotlin.Core.KOTLIN_REFLECT)
-    implementation(Deps.Android.Arch.Core.TESTING)
-    implementation(Deps.Android.Test.Espresso.CORE)
 
     detektPlugins(Plugins.DETEKT_FORMATTING)
 }
