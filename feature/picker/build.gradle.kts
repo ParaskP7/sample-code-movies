be362c49bebf0fd9eaeb69a2a8c2bb4f8716fd95
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-06-02 16:31:36 +0300

    [movies] build: Configure picker based on deps analysis advise

--- feature/picker/build.gradle.kts
+++ feature/picker/build.gradle.kts
@@ -1,6 +1,7 @@
 @file:Suppress("InvalidPackageDeclaration")
 
 import io.petros.movies.config.deps.Deps
+import io.petros.movies.config.deps.identifier
 
 plugins {
     id(Plugins.Id.Android.LIBRARY)
@@ -9,11 +10,28 @@ plugins {
     id(Plugins.Id.Dependency.VERSIONS)
 }
 
+dependencyAnalysis {
+    issues {
+        onUnusedDependencies {
+            exclude(
+                Deps.Project.Implementation.Android.Core.CORE, // Ignore remove advise. Required for theme purposes.
+                Deps.Android.Core.APP_COMPAT.identifier() // Ignore remove advise. Otherwise, 'AlertDialog' is unresolved.
+            )
+        }
+        onIncorrectConfiguration {
+            exclude(
+                Deps.Android.Core.FRAGMENT.identifier() // Ignore change to 'api' advice.
+            )
+        }
+    }
+}
+
 dependencies {
     implementation(project(Deps.Project.Implementation.Android.Core.CORE))
 
     implementation(Deps.Kotlin.Core.KOTLIN)
     implementation(Deps.Android.Core.APP_COMPAT)
+    implementation(Deps.Android.Core.FRAGMENT)
     api(Deps.Util.MONTH_YEAR_PICKER) // Usage of api is required because of 'MovieActivity', which cannot access it.
 
     detektPlugins(Plugins.DETEKT_FORMATTING)
