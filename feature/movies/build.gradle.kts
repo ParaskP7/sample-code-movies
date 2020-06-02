14d11b092e89882027fea97d8031532125f9c9be
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-06-02 16:32:07 +0300

    [movies] build: Configure movies based on deps analysis advise

--- feature/movies/build.gradle.kts
+++ feature/movies/build.gradle.kts
@@ -1,6 +1,7 @@
 @file:Suppress("InvalidPackageDeclaration")
 
 import io.petros.movies.config.deps.Deps
+import io.petros.movies.config.deps.identifier
 
 plugins {
     id(Plugins.Id.Android.LIBRARY)
@@ -13,6 +14,22 @@ plugins {
     id(Plugins.Id.Test.COVERAGE)
 }
 
+dependencyAnalysis {
+    issues {
+        onIncorrectConfiguration {
+            exclude(
+                Deps.Project.Implementation.Kotlin.DOMAIN, // Ignore change to 'api' advice.
+                Deps.Project.Implementation.Android.Core.CORE, // Ignore change to 'api' advice.
+                Deps.Material.MATERIAL.identifier(), // Ignore change to 'api' advice.
+                Deps.Android.Core.CONSTRAINT_LAYOUT.identifier(), // Ignore change to 'api' advice.
+                Deps.Android.Core.COORDINATOR_LAYOUT.identifier(), // Ignore change to 'api' advice.
+                Deps.Android.Core.RECYCLER_VIEW.identifier(), // Ignore change to 'api' advice.
+                Deps.Architecture.Mvi.STATEFUL.identifier() // Ignore change to 'api' advice.
+            )
+        }
+    }
+}
+
 dependencies {
     implementation(project(Deps.Project.Implementation.Kotlin.UTILS))
     implementation(project(Deps.Project.Implementation.Kotlin.DOMAIN))
@@ -23,17 +40,21 @@ dependencies {
     implementation(Deps.Kotlin.Core.KOTLIN)
     implementation(Deps.Kotlin.Coroutines.CORE)
     implementation(Deps.Material.MATERIAL)
-    implementation(Deps.Android.Core.APP_COMPAT)
+    implementation(Deps.Android.Core.FRAGMENT)
+    implementation(Deps.Android.Core.RECYCLER_VIEW)
     implementation(Deps.Android.Core.CONSTRAINT_LAYOUT)
+    implementation(Deps.Android.Core.COORDINATOR_LAYOUT)
     implementation(Deps.Android.Ktx.CORE)
-    implementation(Deps.Android.Arch.Lifecycle.LIVE_DATA)
+    implementation(Deps.Android.Arch.Lifecycle.LIVE_DATA_CORE)
     implementation(Deps.Android.Arch.Lifecycle.VIEW_MODEL)
     implementation(Deps.Android.Arch.Lifecycle.VIEW_MODEL_KTX)
+    implementation(Deps.Android.Arch.Navigation.COMMON)
+    implementation(Deps.Android.Arch.Navigation.COMMON_KTX)
+    implementation(Deps.Android.Arch.Navigation.RUNTIME)
     implementation(Deps.Android.Arch.Navigation.FRAGMENT_KTX)
-    implementation(Deps.Android.Arch.Navigation.UI_KTX)
     implementation(Deps.Architecture.Mvi.STATEFUL)
     kapt(Deps.Architecture.Mvi.STATEFUL_COMPILER)
-    implementation(Deps.Di.Koin.Android.ANDROID)
+    implementation(Deps.Di.Koin.Kotlin.CORE)
     implementation(Deps.Di.Koin.Android.VIEW_MODEL)
     implementation(Deps.Log.TIMBER)
 
