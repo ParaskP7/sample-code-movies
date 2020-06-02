51a6ad728befb1ecbc08a14e8d39abef2e9e6ae9
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-06-02 16:25:22 +0300

    [movies] build: Configure test based on deps analysis advise

--- test/build.gradle.kts
+++ test/build.gradle.kts
@@ -1,11 +1,26 @@
 @file:Suppress("InvalidPackageDeclaration")
 
 import io.petros.movies.config.deps.Deps
+import io.petros.movies.config.deps.identifier
 
 plugins {
     id(Plugins.Id.Kotlin.KOTLIN)
     id(Plugins.Id.Quality.DETEKT)
     id(Plugins.Id.Dependency.VERSIONS)
+}
+
+dependencyAnalysis {
+    issues {
+        onIncorrectConfiguration {
+            exclude(
+                Deps.Project.Implementation.Kotlin.DOMAIN, // Ignore change to 'api' advice.
+                Deps.Kotlin.Coroutines.Test.TEST.identifier(), // Ignore change to 'api' advice.
+                Deps.Test.JUnit.J_UNIT_4.identifier(), // Ignore change to 'api' advice.
+                Deps.Test.Spek.DSL.identifier(), // Ignore change to 'api' advice.
+                Deps.Test.Integration.MOCK_WEB_SERVER.identifier() // Ignore change to 'api' advice.
+            )
+        }
+    }
 }
 
 dependencies {
@@ -14,6 +29,8 @@ dependencies {
     implementation(Deps.Kotlin.Core.KOTLIN)
     implementation(Deps.Kotlin.Coroutines.CORE)
     implementation(Deps.Kotlin.Coroutines.Test.TEST)
+    implementation(Deps.Net.OkHttp.OK_HTTP)
+    implementation(Deps.Net.Gson.GSON)
     implementation(Deps.Net.Rest.RETROFIT)
     implementation(Deps.Net.Rest.RETROFIT_GSON)
     implementation(Deps.Test.JUnit.J_UNIT_4)
