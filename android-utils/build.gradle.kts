113479b98aa89e25b78160d28f695a352b7cdc42
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-06-02 16:26:29 +0300

    [movies] build: Configure android utils based on deps analysis advise

--- android-utils/build.gradle.kts
+++ android-utils/build.gradle.kts
@@ -1,6 +1,7 @@
 @file:Suppress("InvalidPackageDeclaration")
 
 import io.petros.movies.config.deps.Deps
+import io.petros.movies.config.deps.identifier
 
 plugins {
     id(Plugins.Id.Android.LIBRARY)
@@ -9,11 +10,23 @@ plugins {
     id(Plugins.Id.Dependency.VERSIONS)
 }
 
+dependencyAnalysis {
+    issues {
+        onIncorrectConfiguration {
+            exclude(
+                Deps.Android.Arch.Lifecycle.COMMON.identifier(), // Ignore change to 'api' advice.
+                Deps.Android.Arch.Lifecycle.LIVE_DATA_CORE.identifier() // Ignore change to 'api' advice.
+            )
+        }
+    }
+}
+
 dependencies {
     implementation(project(Deps.Project.Implementation.Kotlin.UTILS))
 
     implementation(Deps.Kotlin.Core.KOTLIN)
-    implementation(Deps.Android.Arch.Lifecycle.LIVE_DATA)
+    implementation(Deps.Android.Arch.Lifecycle.COMMON)
+    implementation(Deps.Android.Arch.Lifecycle.LIVE_DATA_CORE)
     implementation(Deps.Log.TIMBER)
 
     detektPlugins(Plugins.DETEKT_FORMATTING)
