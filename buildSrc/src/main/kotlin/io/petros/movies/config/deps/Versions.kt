33d3920820a0df9aa2646a4062736017f7e61767
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-06-30 17:36:48 +0300

    [movies] update: Update drawer layout to latest 1.1.0 version

--- buildSrc/src/main/kotlin/io/petros/movies/config/deps/Versions.kt
+++ buildSrc/src/main/kotlin/io/petros/movies/config/deps/Versions.kt
@@ -46,7 +46,7 @@ object Versions {
             const val CONSTRAINT_LAYOUT = "2.0.0-beta7" // Released: 12-06-20
 
             // Releases: https://developer.android.com/jetpack/androidx/releases/drawerlayout
-            const val DRAWER_LAYOUT = "1.0.0" // Released: 21-09-18
+            const val DRAWER_LAYOUT = "1.1.0" // Released: 24-06-20
 
             // Releases: https://developer.android.com/jetpack/androidx/releases/coordinatorlayout
             const val COORDINATOR_LAYOUT = "1.1.0" // Released: 04-12-19
