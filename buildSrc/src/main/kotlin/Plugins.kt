0ba9a7af14f20d683910f8780d0d891cfa9e869a
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-07-09 14:21:08 +0300

    [movies] docs: Update android studio to beta 4.1.0-beta03 version
    This change is done only for documentation purposes.

--- buildSrc/src/main/kotlin/Plugins.kt
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -12,7 +12,7 @@ object Plugins {
 
         // Releases: https://androidstudio.googleblog.com
         @Suppress("unused") const val ANDROID = "4.0.0" // Released: 28-05-20
-        @Suppress("unused") const val ANDROID_BETA = "4.1.0-beta02" // Released: 25-06-20
+        @Suppress("unused") const val ANDROID_BETA = "4.1.0-beta03" // Released: 07-07-20
         const val ANDROID_CANARY = "4.2.0-alpha04" // Released: 08-07-20
 
         // Releases: https://github.com/GradleUp/auto-manifest/releases
