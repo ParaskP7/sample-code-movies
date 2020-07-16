40e582730cec28a58959bc0a4d7c7c4580604962
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-07-16 18:48:08 +0300

    [movies-27] feature: Implement load movies stream in movies repository
    The implementation of 'loadMoviesStream(...)' is paging library specific
    and in general is composed of 3 main input and 1 output element:
    - Input:
      1) PagingConfig: This is the paging configuration and helps with
         defining things like 'pageSize', 'placeholders', etc.
      2) RemoteMediator: This was explained before, it is the class
         responsible to mediate between the web and database calls.
      3) PagingSourceFactory: This is where the source of truth is defined,
         in terms of where the stream of data is coming from in the end.
    - Output:
      1) The output data is a flow of movie paging data, but for this to
         work the list of movie entities, which is returned from the source
         of truth, was mapped to a list of movies.

--- data/src/main/kotlin/io/petros/movies/data/repository/movie/MoviesRepositoryImpl.kt
+++ data/src/main/kotlin/io/petros/movies/data/repository/movie/MoviesRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package io.petros.movies.data.repository.movie
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import io.petros.movies.database.MoviesDatabase
 import io.petros.movies.domain.model.asResult
@@ -7,7 +9,9 @@ import io.petros.movies.domain.model.movie.Movie
 import io.petros.movies.domain.repository.movie.MoviesRepository
 import io.petros.movies.network.WebService
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
+@Suppress("ForbiddenComment")
 class MoviesRepositoryImpl(
     private val webService: WebService,
     private val database: MoviesDatabase,
@@ -17,8 +21,23 @@ class MoviesRepositoryImpl(
         webService.loadMovies(year, month, page)
     }
 
+    // TODO: Figure out a way to add an appropriate test for this function.
     override suspend fun loadMoviesStream(year: Int?, month: Int?): Flow<PagingData<Movie>> {
-        TODO()
+        return Pager(
+            config = PagingConfig(
+                pageSize = MoviesRemoteMediator.MOVIES_PAGE_SIZE,
+                enablePlaceholders = false
+            ),
+            remoteMediator = MoviesRemoteMediator(
+                webService = webService,
+                database = database,
+                year = year,
+                month = month,
+            ),
+            pagingSourceFactory = { database.moviesDao().movies() }
+        ).flow.map { pagingData ->
+            pagingData.map { it.toMovie() }
+        }
     }
 
     override suspend fun loadMovie(id: Int) = asResult {
