635497a5acb2b044f6c7854f92aaeb76f50f235a
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-07-21 15:04:37 +0300

    [movies-27] cleanup: Remove unused pagination data domain data class
    The 'MoviesPage' data class no longer requires to extend the
    'PaginationData' data class, since all that are now handled by the
    paging library automatically.

--- domain/src/main/kotlin/io/petros/movies/domain/model/movie/MoviesPage.kt
+++ domain/src/main/kotlin/io/petros/movies/domain/model/movie/MoviesPage.kt
@@ -1,8 +1,6 @@
 package io.petros.movies.domain.model.movie
 
-import io.petros.movies.domain.model.common.PaginationData
-
 data class MoviesPage(
-    override val nextPage: Int?,
-    override val items: List<Movie>,
-) : PaginationData.InfiniteScrollPage<Movie>
+    val nextPage: Int?,
+    val items: List<Movie>,
+)
