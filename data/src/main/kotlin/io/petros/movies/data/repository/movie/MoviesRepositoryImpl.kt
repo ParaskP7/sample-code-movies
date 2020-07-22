24adb4dd065b9aa0c7a4abb97bed176dc4cb7b8e
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-07-22 19:41:46 +0300

    [movies-27] refactor: Extract pager construction to a private function

--- data/src/main/kotlin/io/petros/movies/data/repository/movie/MoviesRepositoryImpl.kt
+++ data/src/main/kotlin/io/petros/movies/data/repository/movie/MoviesRepositoryImpl.kt
@@ -2,14 +2,11 @@ package io.petros.movies.data.repository.movie
 
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
-import androidx.paging.PagingData
 import io.petros.movies.database.MoviesDatabase
 import io.petros.movies.database.entity.toDate
 import io.petros.movies.domain.model.asResult
-import io.petros.movies.domain.model.movie.Movie
 import io.petros.movies.domain.repository.movie.MoviesRepository
 import io.petros.movies.network.MoviesService
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 @Suppress("ForbiddenComment")
@@ -22,24 +19,24 @@ class MoviesRepositoryImpl(
         database.moviesDao().firstMovie().toDate()
     }
 
-    // TODO: Figure out a way to add an appropriate test for this function.
-    override suspend fun loadMoviesStream(year: Int?, month: Int?): Flow<PagingData<Movie>> {
-        return Pager(
-            config = PagingConfig(
-                pageSize = MoviesRemoteMediator.MOVIES_PAGE_SIZE,
-                enablePlaceholders = false
-            ),
-            remoteMediator = MoviesRemoteMediator(
-                service = service,
-                database = database,
-                year = year,
-                month = month,
-            ),
-            pagingSourceFactory = { database.moviesDao().movies() }
-        ).flow.map { pagingData ->
-            pagingData.map { it.toMovie() }
-        }
+    // TODO: Figure out a way to test this function.
+    override suspend fun loadMoviesStream(year: Int?, month: Int?) = pager(year, month).map { pagingData ->
+        pagingData.map { it.toMovie() }
     }
+
+    private fun pager(year: Int?, month: Int?) = Pager(
+        config = PagingConfig(
+            pageSize = MoviesRemoteMediator.MOVIES_PAGE_SIZE,
+            enablePlaceholders = false
+        ),
+        remoteMediator = MoviesRemoteMediator(
+            service = service,
+            database = database,
+            year = year,
+            month = month,
+        ),
+        pagingSourceFactory = { database.moviesDao().movies() }
+    ).flow
 
     override suspend fun loadMovieStream(id: Int) = database.moviesDao().movie(id)
         .map { asResult { it.toMovie() } }
