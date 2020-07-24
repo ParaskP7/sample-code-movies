0e87f12f702139afe44d7be369fde7cf7ab8fec0
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-07-24 11:09:58 +0300

    [movies-27] build: Ignore change to api advise for database module

--- database/build.gradle.kts
+++ database/build.gradle.kts
@@ -2,6 +2,7 @@
 
 import io.petros.movies.config.deps.Deps
 import io.petros.movies.config.deps.Projects
+import io.petros.movies.config.deps.identifier
 
 plugins {
     id(Plugins.Id.Android.LIBRARY)
@@ -26,4 +27,19 @@ dependencies {
     implementation(Deps.Di.Koin.Kotlin.CORE)
 
     detektPlugins(Plugins.DETEKT_FORMATTING)
+}
+
+dependencyAnalysis {
+    issues {
+        onIncorrectConfiguration {
+            exclude(
+                Projects.Implementation.Kotlin.DOMAIN, // Ignore change to 'api' advice.
+                Deps.Kotlin.Core.KOTLIN.identifier(), // Ignore change to 'api' advice.
+                Deps.Kotlin.Coroutines.CORE_JVM.identifier(), // Ignore change to 'api' advice.
+                Deps.Android.Arch.Room.RUNTIME.identifier(), // Ignore change to 'api' advice.
+                Deps.Android.Arch.Pagination.COMMON.identifier(), // Ignore change to 'api' advice.
+                Deps.Di.Koin.Kotlin.CORE.identifier() // Ignore change to 'api' advice.
+            )
+        }
+    }
 }
