72d151f0192ec8f5a88cc0c99baf58536ed00c68
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-08-06 16:39:52 +0300

    [movies] update: Update constraint layout to rc 2.0.0-rc1 version

--- buildSrc/src/main/kotlin/io/petros/movies/config/deps/Versions.kt
+++ buildSrc/src/main/kotlin/io/petros/movies/config/deps/Versions.kt
@@ -44,7 +44,7 @@ object Versions {
 
             // Releases: https://developer.android.com/jetpack/androidx/releases/constraintlayout
             @Suppress("unused") const val CONSTRAINT_LAYOUT = "1.1.3" // Released: 16-12-19
-            const val CONSTRAINT_LAYOUT_BETA = "2.0.0-beta8" // Released: 07-07-20
+            const val CONSTRAINT_LAYOUT_RC = "2.0.0-rc1" // Released: 29-07-20
 
             // Releases: https://developer.android.com/jetpack/androidx/releases/drawerlayout
             const val DRAWER_LAYOUT = "1.1.0" // Released: 24-06-20
