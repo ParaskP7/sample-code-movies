4b89fbfb1a435538d790ebd60dcf3ff42e54fce7
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-08-06 17:41:50 +0300

    [movies] update: Update paging to alpha 3.0.0-alpha04 version

--- buildSrc/src/main/kotlin/io/petros/movies/config/deps/Versions.kt
+++ buildSrc/src/main/kotlin/io/petros/movies/config/deps/Versions.kt
@@ -120,7 +120,7 @@ object Versions {
 
                 // Releases: https://developer.android.com/jetpack/androidx/releases/paging
                 @Suppress("unused") const val PAGING = "2.1.2" // Release: 18-03-20
-                const val PAGING_ALPHA = "3.0.0-alpha03" // Release: 22-07-20
+                const val PAGING_ALPHA = "3.0.0-alpha04" // Release: 05-08-20
 
             }
 
