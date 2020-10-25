b76d99713e07b04d86332552861d60485918da44
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-10-25 16:35:13 +0200

    [movies] update: Update junit4 to latest 4.13.1 version

--- buildSrc/src/main/kotlin/io/petros/movies/config/deps/Versions.kt
+++ buildSrc/src/main/kotlin/io/petros/movies/config/deps/Versions.kt
@@ -234,7 +234,7 @@ object Versions {
         object JUnit {
 
             // Releases: https://github.com/junit-team/junit4/releases
-            const val J_UNIT_4 = "4.13" // Released: 01-01-20
+            const val J_UNIT_4 = "4.13.1" // Released: 11-10-20
 
             // Releases: https://github.com/junit-team/junit5/releases
             const val J_UNIT_5 = "5.7.0" // Released: 13-09-20
