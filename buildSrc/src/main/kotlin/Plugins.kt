80fba6f34a1463b717351f9ccadf761f53bd3aac
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-11-22 17:24:36 +0200

    [movies] update: Update dependency analysis plugin to latest 0.66.0

--- buildSrc/src/main/kotlin/Plugins.kt
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -26,7 +26,7 @@ object Plugins {
         const val DEPENDENCY_VERSIONS = "0.36.0" // Released: 09-11-20
 
         // Releases: https://github.com/autonomousapps/dependency-analysis-android-gradle-plugin/releases
-        const val DEPENDENCY_ANALYSIS = "0.65.0" // Released: 10-11-20
+        const val DEPENDENCY_ANALYSIS = "0.66.0" // Released: 19-11-20
 
         // Releases: https://github.com/runningcode/gradle-doctor/releases
         const val GRADLE_DOCTOR = "0.6.3" // Released: 26-10-20
