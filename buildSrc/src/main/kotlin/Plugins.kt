31e0039ff872694b643e2fcda98407a3929921e7
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-11-28 14:54:19 +0200

    [movies] update: Update kotlin to latest stable 1.4.20 version

--- buildSrc/src/main/kotlin/Plugins.kt
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -7,7 +7,7 @@ object Plugins {
     object Version {
 
         // Releases: https://blog.jetbrains.com/kotlin/category/releases
-        const val KOTLIN = "1.4.10" // Released: 10-09-20
+        const val KOTLIN = "1.4.20" // Released: 12-11-20
 
         // Releases: https://androidstudio.googleblog.com
         @Suppress("unused") const val ANDROID = "4.1.1" // Released: 10-11-20
