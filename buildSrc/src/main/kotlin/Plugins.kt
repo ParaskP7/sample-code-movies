aff597fb152c7ae96564c751b3f2133a8ed51a7e
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2020-12-14 06:10:28 +0200

    [movies] update: Update gradle doctor plugin to latest 0.7.0 version
    As part of this update the 'UseParallelGC' is added to Gradle JVM
    arguments. This is now recommended by the Gradle Doctor plugin for Java
    9+.

--- buildSrc/src/main/kotlin/Plugins.kt
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -29,7 +29,7 @@ object Plugins {
         const val DEPENDENCY_ANALYSIS = "0.69.0" // Released: 05-12-20
 
         // Releases: https://github.com/runningcode/gradle-doctor/releases
-        const val GRADLE_DOCTOR = "0.6.3" // Released: 26-10-20
+        const val GRADLE_DOCTOR = "0.7.0" // Released: 04-10-20
 
         // Releases: https://github.com/jraska/modules-graph-assert/releases
         const val MODULE_GRAPH_ASSERT = "1.4.0" // Released: 17-08-20
