49160e972755ea48f844a302bf2c785336cc0259
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2021-01-10 11:44:53 +0200

    [movies] update: Update gradle android plugin to 7.0.0-alpha04 version

--- buildSrc/src/main/kotlin/Plugins.kt
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -12,7 +12,7 @@ object Plugins {
         // Releases: https://androidstudio.googleblog.com
         @Suppress("unused") const val ANDROID_GRADLE = "4.1.1" // Released: 10-11-20
         @Suppress("unused") const val ANDROID_GRADLE_BETA = "4.2.0-beta02" // Released: 14-12-20
-        const val ANDROID_GRADLE_CANARY = "7.0.0-alpha03" // Released: 14-12-20
+        const val ANDROID_GRADLE_CANARY = "7.0.0-alpha04" // Released: 06-01-21
 
         // Releases: https://github.com/GradleUp/auto-manifest/releases
         const val ANDROID_MANIFEST = "1.0.4" // Released: 13-11-20
