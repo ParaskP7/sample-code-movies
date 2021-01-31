6478ea2c25516c120f77816eb48bc9c3aafc8523
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2021-01-31 21:09:14 +0200

    [movies] update: Update glide to latest 4.12.0 version

--- buildSrc/src/main/kotlin/io/petros/movies/config/deps/Versions.kt
+++ buildSrc/src/main/kotlin/io/petros/movies/config/deps/Versions.kt
@@ -215,7 +215,7 @@ object Versions {
         object Glide {
 
             // Releases: https://github.com/bumptech/glide/releases
-            const val GLIDE = "4.11.0" // Released: 09-01-20
+            const val GLIDE = "4.12.0" // Released: 29-01-21
 
         }
 
