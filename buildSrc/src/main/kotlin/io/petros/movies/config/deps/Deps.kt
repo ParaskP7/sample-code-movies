6b42cab03a8f6de2c19035b99bbb5673fdaa15e1
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2021-02-13 21:25:39 +0200

    [movies] update: Update saved state to latest 1.1.0 version

--- buildSrc/src/main/kotlin/io/petros/movies/config/deps/Deps.kt
+++ buildSrc/src/main/kotlin/io/petros/movies/config/deps/Deps.kt
@@ -107,7 +107,7 @@ object Deps {
 
             object SavedState {
 
-                val SAVED_STATE = "androidx.savedstate:savedstate:${Versions.Android.Arch.SavedState.SAVED_STATE_RC}"
+                val SAVED_STATE = "androidx.savedstate:savedstate:${Versions.Android.Arch.SavedState.SAVED_STATE}"
 
             }
 
