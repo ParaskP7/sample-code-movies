79dafd21ffb0edff4a4587c00cb42220ce95b776
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2021-03-14 21:51:33 +0200

    [movies] update: Update gradle android plugin to 7.0.0-alpha09 version
    This version of AGP has a 'Robolectric' bug, which is reported here:
    https://github.com/robolectric/robolectric/issues/6277
    
    Until the bug is fix, please running 'gradlew test' will fail all
    Robolectfict related tests.

--- buildSrc/src/main/kotlin/Plugins.kt
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -12,7 +12,7 @@ object Plugins {
         // Releases: https://androidstudio.googleblog.com
         @Suppress("unused") const val ANDROID_GRADLE = "4.1.2" // Released: 19-01-21
         @Suppress("unused") const val ANDROID_GRADLE_BETA = "4.2.0-beta05" // Released: 23-02-21
-        const val ANDROID_GRADLE_CANARY = "7.0.0-alpha08" // Released: 24-02-21
+        const val ANDROID_GRADLE_CANARY = "7.0.0-alpha09" // Released: 24-02-21
 
         // Releases: https://github.com/GradleUp/auto-manifest/releases
         const val ANDROID_MANIFEST = "1.0.4" // Released: 13-11-20
