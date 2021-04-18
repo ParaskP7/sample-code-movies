56c8c4691426764ca43cd89deeb760e76dadd701
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2021-04-18 21:24:09 +0300

    [movies-29] feature: Add drawer and jetpack compose menu item switch
    This 'Jetpack Compose' menu item switch will be later on be used to
    change the screens from using the legacy Android View UI system to using
    the new modern Jetpack Compose UI.

--- app/src/main/kotlin/io/petros/movies/app/AppActivity.kt
+++ app/src/main/kotlin/io/petros/movies/app/AppActivity.kt
@@ -1,19 +1,21 @@
 package io.petros.movies.app
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.switchmaterial.SwitchMaterial
 import io.petros.movies.R
 import io.petros.movies.android_utils.network.NetworkLiveEvent
 import io.petros.movies.core.activity.BaseActivity
 import io.petros.movies.core.view_binding.viewBinding
 import io.petros.movies.databinding.AppActivityBinding
 
-@Suppress("GoogleAppIndexingApiWarning")
+@Suppress("GoogleAppIndexingApiWarning", "TooManyFunctions")
 class AppActivity : BaseActivity<AppActivityBinding>() {
 
     override val binding by viewBinding(AppActivityBinding::inflate)
@@ -36,6 +38,7 @@ class AppActivity : BaseActivity<AppActivityBinding>() {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         initActionBar()
+        initDrawer()
         initObservers()
     }
 
@@ -43,6 +46,26 @@ class AppActivity : BaseActivity<AppActivityBinding>() {
         setSupportActionBar(binding.toolbar)
         val navController = findNavController(R.id.navHostFragment)
         setupActionBarWithNavController(navController, configuration)
+    }
+
+    private fun initDrawer() {
+        initComposeMenuItem(binding.navigationView.menu.findItem(R.id.drawerMenuItemJetpackCompose))
+    }
+
+    private fun initComposeMenuItem(menuItem: MenuItem) {
+        val switch = menuItem.actionView as SwitchMaterial
+        initComposeMenuItemSwitchState(switch)
+        initComposeMenuItemSwitchListener(switch)
+    }
+
+    @Suppress("ForbiddenComment", "UnusedPrivateMember")
+    private fun initComposeMenuItemSwitchState(@Suppress("UNUSED_PARAMETER") switch: SwitchMaterial) {
+        // TODO: Implement settings.
+    }
+
+    @Suppress("FunctionMaxLength", "ForbiddenComment", "UnusedPrivateMember")
+    private fun initComposeMenuItemSwitchListener(@Suppress("UNUSED_PARAMETER") switch: SwitchMaterial) {
+        // TODO: Implement settings.
     }
 
     private fun initObservers() {
