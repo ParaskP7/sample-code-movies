1e9410ab6fac1f6af9ed6b334ada7f931e547e53
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2021-06-06 22:16:02 +0300

    [movies] update: Update recycler view to latest 1.2.1 version

--- buildSrc/src/main/kotlin/io/petros/movies/config/deps/Versions.kt
+++ buildSrc/src/main/kotlin/io/petros/movies/config/deps/Versions.kt
@@ -41,7 +41,7 @@ object Versions {
             const val FRAGMENT = "1.3.4" // Released: 18-05-21
 
             // Releases: https://developer.android.com/jetpack/androidx/releases/recyclerview
-            const val RECYCLER_VIEW = "1.2.0" // Released: 07-04-21
+            const val RECYCLER_VIEW = "1.2.1" // Released: 02-06-21
 
             // Releases: https://developer.android.com/jetpack/androidx/releases/constraintlayout
             @Suppress("unused") const val CONSTRAINT_LAYOUT = "2.0.4" // Released: 29-10-20
