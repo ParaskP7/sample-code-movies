4e4fb19394aee4f5d184ca108ccad878573a3408
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2021-12-04 17:24:36 +0200

    [movies] test: Change filter and picker month from november to december
    The TODOs on that matter should fix the problem once and for all.
    However, all in due time

--- app/src/androidTest/kotlin/io/petros/movies/app/robot/app/movies/MoviesToolbarRobot.kt
+++ app/src/androidTest/kotlin/io/petros/movies/app/robot/app/movies/MoviesToolbarRobot.kt
@@ -28,7 +28,7 @@ class MoviesToolbarRobot : ScopedActions(idMatcher(R.id.toolbar)) {
 
         // TODO: Revert to 'April' by specifically selecting this month during testing
         @Suppress("unused") const val MOVIES_TOOLBAR_FILTER_MONTH_APRIL = "April"
-        const val MOVIES_TOOLBAR_FILTER_MONTH_VALUE = "November"
+        const val MOVIES_TOOLBAR_FILTER_MONTH_VALUE = "December"
 
     }
 
