81327d8a785a8d1cfe5fc3224f3c14b0ba0599fa
Petros Paraskevopoulos <petros.g.paraskevopoulos@gmail.com>
2021-12-18 23:19:41 +0200

    [movies] update: Update material to 1.5.0-rc01 version

--- buildSrc/src/main/kotlin/io/petros/movies/config/deps/Versions.kt
+++ buildSrc/src/main/kotlin/io/petros/movies/config/deps/Versions.kt
@@ -27,7 +27,7 @@ object Versions {
 
         // Releases: https://github.com/material-components/material-components-android/releases
         @Suppress("unused") const val MATERIAL = "1.4.0" // Released: 02-07-21
-        const val MATERIAL_BETA = "1.5.0-beta01" // Released: 11-11-21
+        const val MATERIAL_RC = "1.5.0-rc01" // Released: 13-12-21
 
     }
 
