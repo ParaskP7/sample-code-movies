f3f066eabcaf2fdb277c26786235bad72af881a0
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2022-01-16 16:17:40 +0100

    [movies] update: Update constraint layout to 2.1.3 version

--- buildSrc/src/main/kotlin/io/petros/movies/config/deps/Versions.kt
+++ buildSrc/src/main/kotlin/io/petros/movies/config/deps/Versions.kt
@@ -43,7 +43,7 @@ object Versions {
             const val RECYCLER_VIEW = "1.2.1" // Released: 02-06-21
 
             // Releases: https://developer.android.com/jetpack/androidx/releases/constraintlayout
-            const val CONSTRAINT_LAYOUT = "2.1.2" // Released: 16-11-21
+            const val CONSTRAINT_LAYOUT = "2.1.3" // Released: 14-01-22
 
             // Releases: https://developer.android.com/jetpack/androidx/releases/drawerlayout
             const val DRAWER_LAYOUT = "1.1.1" // Released: 02-09-20
