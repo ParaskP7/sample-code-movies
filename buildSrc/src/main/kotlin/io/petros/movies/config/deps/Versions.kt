49782df885fc45414f53e91dd1638028e45ee5f5
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2022-06-13 12:10:42 +0300

    [movies] update: Update coroutines to 1.6.2 version

--- buildSrc/src/main/kotlin/io/petros/movies/config/deps/Versions.kt
+++ buildSrc/src/main/kotlin/io/petros/movies/config/deps/Versions.kt
@@ -17,7 +17,7 @@ object Versions {
     object Kotlin {
 
         // Releases: https://github.com/Kotlin/kotlinx.coroutines/releases
-        const val COROUTINES = "1.6.1" // Released: 04-04-22
+        const val COROUTINES = "1.6.2" // Released: 27-05-22
 
     }
 
