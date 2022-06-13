67e9049c42d7d282572263a754edb7755d6cee85
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2022-06-13 12:30:51 +0300

    [movies] update: Update navigation to 2.5.0-rc01 version

--- buildSrc/src/main/kotlin/io/petros/movies/config/deps/Versions.kt
+++ buildSrc/src/main/kotlin/io/petros/movies/config/deps/Versions.kt
@@ -110,7 +110,7 @@ object Versions {
 
                 // Releases: https://developer.android.com/jetpack/androidx/releases/navigation
                 @Suppress("unused") const val NAVIGATION = "2.4.1" // Released: 23-02-22
-                const val NAVIGATION_BETA = "2.5.0-beta01" // Released: 20-04-22
+                const val NAVIGATION_RC = "2.5.0-rc01" // Released: 11-05-22
 
             }
 
