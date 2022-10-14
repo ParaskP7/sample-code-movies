766b13117f8b0ac4089bfa402171f7badf35f7d1
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2022-10-14 21:54:16 +0300

    [movies] test: Change filter and picker month from september to october
    The TODOs on that matter should fix the problem once and for all.
    However, all in due time.

--- app/src/androidTest/kotlin/io/petros/movies/app/robot/app/picker/MonthPickerRobot.kt
+++ app/src/androidTest/kotlin/io/petros/movies/app/robot/app/picker/MonthPickerRobot.kt
@@ -22,7 +22,7 @@ class MonthPickerRobot : ScopedActions(idMatcher(R.id.picker)) {
 
         // TODO: Revert to original value by specifically selecting the month during testing
         @Suppress("unused") const val MOVIE_MONTH_PICKER_MONTH_APR = "Apr"
-        const val MOVIE_MONTH_PICKER_MONTH_VALUE = "Sep"
+        const val MOVIE_MONTH_PICKER_MONTH_VALUE = "Oct"
 
         private const val ACTION_OK = "OK"
 
