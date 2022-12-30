b2db8e2660b3ab8849106ccaf6fddf6c816e29ae
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2022-12-30 15:46:26 +0200

    [movies] build: Set the kotlin jvm target to java 11 for build logic
    After updating Gradle to '8.0-rc-1', if the 'Kotlin Compile' related
    'JVM Target' option on the 'build-logic' module is not explicitly set
    to target Java 11, then the below exception is thrown:
    
    ------------------------------------------------------------------------
    * What went wrong:
    Execution failed for task ':build-logic:convention:compileKotlin'.
    > 'compileJava' task (current target is 11) and 'compileKotlin' task
     (current target is 17) jvm target compatibility should be set to the
     same Java version.
      Consider using JVM toolchain: https://kotl.in/gradle/jvm/toolchain
    ------------------------------------------------------------------------

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     `kotlin-dsl`
 }
@@ -7,6 +9,12 @@ group = "io.petros.movies.buildlogic"
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
 }
 
 dependencies {
