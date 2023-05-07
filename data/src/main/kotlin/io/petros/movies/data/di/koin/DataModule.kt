b880f17fd80f9d009599f440752cb3659980e85e
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2023-05-07 18:22:55 +0300

    Build: Remove datastore module by commenting it out from everywhere
    The 'Compose Compiler', 'Kotlin' and 'KSP' update, along with the extra
    KSP related 'jvmToolchain' configuration, seems to be causing some
    incompatibility with the 'datastore' module, which in its turn is
    causing the project to fail to compile due to the unresolved references
    below:
    
    ------------------------------------------------------------------------
    > Task :data:compileDebugKotlin FAILED
    e: .../data/src/main/kotlin/io/petros/movies/data/di/koin/
    DataModule.kt:6:35 Unresolved reference: di
    e: .../data/src/main/kotlin/io/petros/movies/data/di/koin/
    DataModule.kt:24:5 Unresolved reference: datastoreModule
    e: .../data/src/main/kotlin/io/petros/movies/data/repository/settings
    /SettingsRepositoryImpl.kt:4:35 Unresolved reference: MoviesDatastore
    e: .../data/src/main/kotlin/io/petros/movies/data/repository/settings/
    SettingsRepositoryImpl.kt:10:28 Unresolved reference: MoviesDatastore
    e: .../data/src/main/kotlin/io/petros/movies/data/repository/settings/
    SettingsRepositoryImpl.kt:14:16 Unresolved reference: it
    e: .../data/src/main/kotlin/io/petros/movies/data/repository/settings/
    SettingsRepositoryImpl.kt:19:13 Unresolved reference: it
    ------------------------------------------------------------------------
    
    As such, this whole 'datastore' module has been temporarily removed, by
    it being commented out from everywhere, with additional TODOs to restore
    its functionality at a later point in the future and in isolation to the
    overall 'Compose Compiler', 'Kotlin' and 'KSP' update.

--- data/src/main/kotlin/io/petros/movies/data/di/koin/DataModule.kt
+++ data/src/main/kotlin/io/petros/movies/data/di/koin/DataModule.kt
@@ -1,19 +1,24 @@
+@file:Suppress("all")
+
 package io.petros.movies.data.di.koin
 
 import io.petros.movies.data.repository.movie.MoviesRepositoryImpl
 import io.petros.movies.data.repository.settings.SettingsRepositoryImpl
 import io.petros.movies.database.di.koin.databaseModule
-import io.petros.movies.datastore.di.koin.datastoreModule
+//import io.petros.movies.datastore.di.koin.datastoreModule
 import io.petros.movies.domain.repository.movie.MoviesRepository
 import io.petros.movies.domain.repository.settings.SettingsRepository
 import io.petros.movies.network.di.koin.networkModule
 import org.koin.dsl.module
 
+// TODO: Revert this!
 private fun repositoryModule() = module {
-    factory<SettingsRepository> { SettingsRepositoryImpl(get()) }
+//    factory<SettingsRepository> { SettingsRepositoryImpl(get()) }
+    factory<SettingsRepository> { SettingsRepositoryImpl() }
     factory<MoviesRepository> { MoviesRepositoryImpl(get(), get()) }
 }
 
+// TODO: Revert this!
 fun dataModule(
     isDebug: Boolean,
     baseUrl: String,
@@ -21,6 +26,6 @@ fun dataModule(
 ) = listOf(
     repositoryModule(),
     networkModule(isDebug, baseUrl, themoviedbApiKey),
-    datastoreModule(),
+//    datastoreModule(),
     databaseModule(),
 )
--- data/src/main/kotlin/io/petros/movies/data/repository/settings/SettingsRepositoryImpl.kt
+++ data/src/main/kotlin/io/petros/movies/data/repository/settings/SettingsRepositoryImpl.kt
@@ -1,23 +1,28 @@
+@file:Suppress("all")
+
 package io.petros.movies.data.repository.settings
 
-import androidx.datastore.preferences.core.edit
-import io.petros.movies.datastore.MoviesDatastore
+//import androidx.datastore.preferences.core.edit
+//import io.petros.movies.datastore.MoviesDatastore
 import io.petros.movies.domain.repository.settings.SettingsRepository
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
+//import kotlinx.coroutines.flow.first
+//import kotlinx.coroutines.flow.map
 
+// TODO: Revert this!
 class SettingsRepositoryImpl(
-    private val datastore: MoviesDatastore,
+//    private val datastore: MoviesDatastore,
 ) : SettingsRepository {
 
-    override suspend fun isComposeEnabled() = datastore.settings().data
-        .map { it[datastore.isComposeEnabled()] == true }
-        .first()
+//    override suspend fun isComposeEnabled() = datastore.settings().data
+//        .map { it[datastore.isComposeEnabled()] == true }
+//        .first()
+    override suspend fun isComposeEnabled() = false
 
-    override suspend fun setComposeEnabled(isEnabled: Boolean) {
-        datastore.settings().edit {
-            it[datastore.isComposeEnabled()] = isEnabled
-        }
-    }
+//    override suspend fun setComposeEnabled(isEnabled: Boolean) {
+//        datastore.settings().edit {
+//            it[datastore.isComposeEnabled()] = isEnabled
+//        }
+//    }
+    override suspend fun setComposeEnabled(isEnabled: Boolean) = Unit
 
 }
